62f96d97f026f6a9d5e5afc28ade202a42423b68
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-09 17:50:40 +0700

    "function doc noi dung tu url"

--- app/src/main/java/com/ptp/phamtanphat/jsonobject0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/jsonobject0405/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ptp.phamtanphat.jsonobject0405;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
         //https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json
         btnReadJson = findViewById(R.id.buttonReadJson);
     }
-    class
+    class ReadJson extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+    private String docNoiDung_Tu_URL(String theUrl){
+        StringBuilder content = new StringBuilder();
+        try    {
+            // create a url object
+            URL url = new URL(theUrl);
+
+            // create a urlconnection object
+            URLConnection urlConnection = url.openConnection();
+
+            // wrap the urlconnection in a bufferedreader
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // read from the urlconnection via the bufferedreader
+            while ((line = bufferedReader.readLine()) != null){
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        }
+        catch(Exception e)    {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
 }
