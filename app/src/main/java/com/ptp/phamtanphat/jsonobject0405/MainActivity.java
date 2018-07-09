04db3c8f33b3b347742f5f90af501c15c200e508
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-09 17:47:19 +0700

    "link demo1"

--- app/src/main/java/com/ptp/phamtanphat/jsonobject0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/jsonobject0405/MainActivity.java
@@ -2,12 +2,18 @@ package com.ptp.phamtanphat.jsonobject0405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnReadJson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json
+        btnReadJson = findViewById(R.id.buttonReadJson);
     }
+    class
 }
